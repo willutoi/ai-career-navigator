c44f3ce4e3bae2e5c87e5148ae37248a0a1683fb
willutoi <baqberqauratuly@gmail.com>
2025-12-01 20:10:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 
 // !!! ВСТАВЬТЕ СЮДА СВОЙ ЛИЧНЫЙ API-КЛЮЧ GEMINI !!!
 // Ключ должен быть внутри двойных кавычек.
-const GEMINI_API_KEY = "ВСТАВЬТЕ_ВАШ_API_КЛЮЧ_СЮДА"; 
+const GEMINI_API_KEY = AIzaSyDlf46A5gJNyHnUp3w_BE1pWTOV-v0hPOs; 
 const API_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=" + GEMINI_API_KEY;
 
 // =======================================================================
@@ -227,3 +227,4 @@ function parseAITextToHTML(aiText) {
 
     return html;
 }
+
